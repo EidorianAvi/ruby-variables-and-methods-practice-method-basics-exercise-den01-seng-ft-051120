8f7c2cfb8d37e2fdd2e4dec8cdbf3e37afde562b
Rene Avila <reneavila1993@gmail.com>
2020-05-14 01:21:19 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  x = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+   puts "#{greeting},#{name}"
 end
 
 def return_a_value
-
+   return "Nice"
 end
 
 def last_evaluated_value
